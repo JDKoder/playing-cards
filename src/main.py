7f9a53d7456425e1ef72fa439e704f2fd2fcd73d
Jacob Krieger <jboy@debee>
2025-08-27 16:12:42 -0400

    Fixed prototype so Ace is an Ace regardless of its race.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,37 @@
 import pydealer as pd
+from pydealer.const import POKER_RANKS
 from player import Player
+
+HIGH_LOW_RANKS = { 
+        "2": 13,
+        "Ace": 12,
+        "King": 11,
+        "Queen": 10,
+        "Jack": 9,
+        "10": 8,
+        "9": 7,
+        "8": 6,
+        "7": 5,
+        "6": 4,
+        "5": 3,
+        "4": 2,
+        "3": 1,
+        "Joker": 0
+}
 
 """This is a prototype for how the game mechanics progress basically."""
 def main():
     #create the game deck preshuffled
     deck = pd.Deck(rebuild=True, re_shuffle=True)
-    #draw a card for the player
-    player_hand = deck.deal(1)
-    #draw a card for the dealer
-    dealer_hand = deck.deal(1)
     #Draw phase
     #Create a player and a dealer that get 1 card each
 
+    #draw a card for the player
+    player_hand = deck.deal(1)
+    print(f"Players card is {player_hand[0]}")
+    #draw a card for the dealer
+    dealer_hand = deck.deal(1)
+    
 
     player = Player(player_hand, 10)
     dealer = Player(dealer_hand, 1000)
@@ -28,19 +48,23 @@ def main():
         player_wager = dealer.credits
         dealer_wager = dealer.credits
 
+    # Finalize Bet phase
+    print(f"player wagers {player_wager}")
     pot += player.take_credits(player_wager)
+    print(f"dealer wagers {dealer_wager}")
     pot += dealer.take_credits(dealer_wager)
 
     #player chooses a to bet "high" or "low"
     player_bet = "high"
-
+    print(f"players bet is {player_bet}")
+    print(f"dealers card is {dealer.hand[0]}")
     #whose card is higher
-    if player.hand[0] > dealer.hand[0]:
+    if player.hand[0].gt(dealer.hand[0], HIGH_LOW_RANKS):
         #player won
         print(f"player wins! {player.hand[0]} is higher than {dealer.hand[0]}")
         print(f"Payout {pot}")
         player.award_credits(pot) 
-    elif player.hand[0] == dealer.hand[0]:
+    elif player.hand[0].eq(dealer.hand[0], HIGH_LOW_RANKS):
         print(f"hand are equal.  That's a push!")
         print(f"Payout {player_wager}")
         player.award_credits(player_wager)
