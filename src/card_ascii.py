9cc2593fd93fff66dd57e3aea75a058e65269b60
JDKoder <jacob.krieger@gmail.com>
2025-08-28 17:07:35 -0400

    adding a card generation file card_ascii.py

---
+++ src/card_ascii.py
@@ -0,0 +1,38 @@
+icon_spade = "󰣑"
+icon_club = "󰣎"
+icon_heart = "󰋑"
+icon_diamond = "󰣏"
+icon_queen = ""
+icon_jack = "󰡘"
+icon_king = "󰆥"
+
+card_top = f" --------- "
+card_bottom = f" --------- "
+card_blank = f"|         |"
+card_top_value = f"| {value}{icon}      |"
+
+value = "A"
+icon = "󰣑"
+#print out a basic Ace card
+print(f" --------- ")
+print(f"| {value}{icon}      |")
+print(f"|         |")
+print(f"|         |")
+print(f"|    {icon}    |")
+print(f"|         |")
+print(f"|         |")
+print(f"|      {icon}{value} |")
+print(f" --------- ")
+
+
+value = "2"
+#print out a basic Ace card
+print(f" --------- ")
+print(f"| {value}{icon}      |")
+print(f"|         |")
+print(f"|         |")
+print(f"|    {icon}    |")
+print(f"|         |")
+print(f"|         |")
+print(f"|      {icon}{value} |")
+print(f" --------- ")
