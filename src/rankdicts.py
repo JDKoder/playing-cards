c952b764691e70b151dd461ea9f828cff6c7b62f
JDKoder <jacob.krieger@gmail.com>
2025-09-26 15:44:54 -0400

    Updating rankdicts for the next game as well as getting the odds_service started

--- src/rankdicts.py
+++ src/rankdicts.py
@@ -28,6 +28,32 @@ BLACKJACK_RANKS = {
         "5": 5,
         "4": 4,
         "3": 3,
-        "2": 2,
-        "Joker": 0
+        "2": 2
 }
+
+WAR_RANKS = { 
+        "Ace": 14,
+        "King": 13,
+        "Queen": 12,
+        "Jack": 11,
+        "10": 10,
+        "9": 9,
+        "8": 8,
+        "7": 7,
+        "6": 6,
+        "5": 5,
+        "4": 4,
+        "3": 3,
+        "2": 2
+}
+
+def card_to_int(card,rank_dict) -> int:
+    return rank_dict[card.value]
+
+def stack_to_int(stack,rank_dict) -> int:
+    accumulator = 0
+    for card in stack:
+        accumulator += card_to_int(card, rank_dict)
+    return accumulator
+
+
